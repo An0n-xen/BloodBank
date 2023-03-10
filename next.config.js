440a13b02e0c5d37b77f528e5e56005ad6a95630
An0n-xen <fyfybaisieamaoh6@gmail.com>
2023-03-10 08:57:08 +0000

    everything

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  env: {
+    MYSQL_HOST: "127.0.0.1",
+    MYSQL_PORT: "3306",
+    MYSQL_DATABASE: "assignments",
+    MYSQL_USER: "root",
+    MYSQL_PASSWORD: "Genesis begins",
+  },
+};
